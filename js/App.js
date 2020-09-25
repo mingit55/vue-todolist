a2d13f26f7d31f6d2f3985973d4e0633dc971ce7
junilhwang <tjsdlf4261@naver.com>
2020-09-25 16:53:21 +0900

    refactor: App.js 리팩토링 중

--- js/App.js
+++ js/App.js
@@ -1,62 +1,50 @@
-window.addEventListener("load", function(){
-    /**
-     * 할 일 목록 컴포넌트
-     */
-    Vue.component('todo', {
-        props: ['item'],
-        template: '<div class="list__item">'
-                        +'<div>'
-                            +'<div class="fs-2">{{item.content}}</div>'
-                            +'<div class="fs-n2 mt-2 text-muted">~{{item.time_limit}}</div>'
-                        +'</div>'
-                        +'<button class="btn-underline">삭제</button>'
-                    +'</div>'
-    });
+import {Todo} from "./components/Todo.js";
 
-    /**
-     * 이벤트 부여
-     */
-
-    document.querySelectorAll(".modal__content")
-        .addEventListener("click", function(e){
-            e.stopPropagation();
-        });
-
-    document.querySelectorAll(".modal")
-        .addEventListener("click", function(e){
-            e.currentTarget.classList.remove("show");
-        });
-    
-        
-
-    /**
-     * App
-     */
-    var app = new Vue({
-        el: "#app",
-        data: {
+const App = {
+    components: { Todo },
+    data () {
+        return {
             todos: [
                 {id: 1, content: "Vue.js 공부하기", time_limit: "2020-09-30"}
             ]
-        },
-        methods: {
-            openModal: function(e){
-                var selector = e.currentTarget.dataset.target;
-                var target = document.querySelector(selector);
-                var inputs = Array.from(target.querySelectorAll("input"));
-                
-                for(i in inputs){
-                    var input = inputs[i];
-                    input.value = "";
-
-                    if(input.dataset.type == "date"){
-                        input.value = new Date().getYmd();
-                        console.log(input, input.value);
-                    }
-                }
-
-                target.classList.add("show");
-            }
         }
-    });
+    },
+    template: `
+        <div>
+            <div class="list">
+    
+                <div class="title">목록</div>
+    
+                <div class="list__content">
+    
+                    <todo v-for="todo in todos"
+                          :item="todo"
+                          :key="todo.id" />
+    
+                    <div class="list__item list__item--insert">
+                        <p>새로운 할 일을 추가할까요?</p>
+                        <button class="btn-underline" :click="openModal" data-target="#insert-form">추가</button>
+                    </div>
+    
+                </div>
+    
+            </div>
+            <div class="view">
+    
+                <div class="title">내용</div>
+    
+                <div class="view__content"></div>
+                
+            </div>
+        </div>
+    `
+}
+
+
+/**
+ * App
+ */
+new Vue({
+    el: "#app",
+    components: { App },
 });
