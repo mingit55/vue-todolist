e93805a647a87368f61c70dcf8c9612e10600f16
junilhwang <tjsdlf4261@naver.com>
2020-09-25 17:37:32 +0900

    refactor: TodoItem에 이벤트 추가

--- js/components/TodoItem.js
+++ js/components/TodoItem.js
@@ -2,17 +2,21 @@
  * 할 일 목록 컴포넌트
  */
 export default {
+
   name: 'TodoItem',
-  props: ['item', 'removeItem'],
+
+  props: ['item'],
+
   template: `
       <div class="list__item">
-          <div>
+          <div @click="$emit('select', item)">
               <div class="fs-2">{{ item.content }}</div>
               <div class="fs-n2 mt-2 text-muted">~{{ item.time_limit }}</div>
           </div>
-          <button class="btn-underline" @click="removeItem(item.id)" type="button">
+          <button class="btn-underline" @click="$emit('remove', item)" type="button">
             삭제
           </button>
       </div>
     `,
+
 };
