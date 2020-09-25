bf636764a8d3b0fa8674b7494eb0f5c64e9bfabb
junilhwang <tjsdlf4261@naver.com>
2020-09-25 17:37:46 +0900

    refactor: App.js에 각종 로직 추가

--- js/App.js
+++ js/App.js
@@ -1,10 +1,11 @@
 import TodoItem from "./components/TodoItem.js";
+import ModalPopup from "./components/ModalPopup.js";
 
-export const App = {
+export default {
 
     name: 'App',
 
-    components: { TodoItem },
+    components: { TodoItem, ModalPopup },
 
     data () {
         return {
@@ -12,6 +13,7 @@ export const App = {
                 {id: 1, content: "Vue.js 공부하기", time_limit: "2020-09-30"}
             ],
             isOpened: false,
+            selectedContent: '',
         }
     },
 
@@ -26,11 +28,13 @@ export const App = {
     
                     <todo-item v-for="todo in todos"
                               :item="todo"
-                              :key="todo.id" />
+                              :key="todo.id"
+                              @select="selectItem"
+                              @remove="removeItem" />
     
                     <div class="list__item list__item--insert">
                         <p>새로운 할 일을 추가할까요?</p>
-                        <button class="btn-underline" :click="openModal" data-target="#insert-form">추가</button>
+                        <button class="btn-underline" @click="openModal" data-target="#insert-form">추가</button>
                     </div>
     
                 </div>
@@ -41,19 +45,54 @@ export const App = {
     
                 <div class="title">내용</div>
     
-                <div class="view__content"></div>
+                <div class="view__content" v-html="selectedContent"></div>
                 
             </div>
             
-            <modal-popup v-if="isOpened"></modal-popup>
+            <modal-popup v-if="isOpened" :close="closeModal" :add-item="addItem" />
             
         </div>
     `,
 
     methods: {
-        openModal (e) {
-            this.isOpened = true
+        openModal () {
+            this.isOpened = true;
+        },
+
+        closeModal () {
+            this.isOpened = false;
+        },
+
+        addItem ({ target }) {
+            const formData = [ ...new FormData(target) ].reduce((data, [key, value]) => {
+                data[key] = value;
+                return data;
+            }, {});
+            if (!formData.content.length) {
+                alert('내용을 입력해주세요');
+                return target.content.focus();
+            }
+            if (!formData.limit_time.length) {
+                alert('날짜를 입력해주세요');
+                return target.limit_time.focus();
+            }
+            this.todos.push({
+                id: this.todos.length + 1,
+                ...formData
+            });
+            alert('추가되었습니다.');
+            this.closeModal();
+        },
+
+        selectItem (todo) {
+            this.selectedContent = todo.content;
+        },
+
+        removeItem (todo) {
+            const itemIndex = this.todos.indexOf(todo);
+            this.todos.splice(itemIndex, 1);
         }
+
     },
 
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.0/dist/vue.esm.browser.js'
-import {App} from './App.js';
+import App from './App.js';
 
 new Vue({
   render: h => h(App)
