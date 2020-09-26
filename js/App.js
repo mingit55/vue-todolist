39061971d7b22eacefebff458110ffc4be8f3d53
mingit55 <mingit55@gmail.com>
2020-09-26 21:38:15 +0900

    Complete 'To do list CRUD'

--- js/App.js
+++ js/App.js
@@ -4,13 +4,21 @@ window.addEventListener("load", function(){
      */
     Vue.component('todo', {
         props: ['item'],
-        template: '<div class="list__item">'
+        template: '<div class="list__item" v-on:click="select">'
                         +'<div>'
                             +'<div class="fs-2">{{item.content}}</div>'
-                            +'<div class="fs-n2 mt-2 text-muted">~{{item.time_limit}}</div>'
+                            +'<div class="fs-n2 mt-2 text-muted">마감 일자&nbsp;&nbsp;&nbsp;{{item.limit}}</div>'
                         +'</div>'
-                        +'<button class="btn-underline">삭제</button>'
-                    +'</div>'
+                        +'<button class="btn-underline" v-on:click.stop="remove">삭제</button>'
+                    +'</div>',
+        methods: {
+            select: function(e){
+                this.$emit('select', this.item);
+            },
+            remove: function(e){
+                this.$emit('remove', this.item.id);
+            }
+        }
     });
 
     /**
@@ -35,27 +43,61 @@ window.addEventListener("load", function(){
     var app = new Vue({
         el: "#app",
         data: {
+            currentView: null,
             todos: [
-                {id: 1, content: "Vue.js 공부하기", time_limit: "2020-09-30"}
+                {id: 1, content: "Vue.js 공부하기", limit: "2020-09-30"}
             ]
         },
         methods: {
+            // 모달 열기
             openModal: function(e){
                 var selector = e.currentTarget.dataset.target;
                 var target = document.querySelector(selector);
-                var inputs = Array.from(target.querySelectorAll("input"));
-                
-                for(i in inputs){
-                    var input = inputs[i];
-                    input.value = "";
 
-                    if(input.dataset.type == "date"){
-                        input.value = new Date().getYmd();
-                        console.log(input, input.value);
-                    }
-                }
+                var content = document.querySelector("#todo__content");
+                var limit = document.querySelector("#todo__limit");
+                
+                content.value = "";
+                limit.value = new Date().getYmd();
 
                 target.classList.add("show");
+            },
+
+            // 모달 닫기
+            closeModal: function(e){
+                var modals = document.querySelectorAll(".modal");
+                for(var i = 0; i < modals.length; i++){
+                    modals.classList.remove("show");
+                }
+            },
+
+            // 할 일 상세보기
+            selectTodo: function(data){
+                this.currentView = data;
+            },
+
+            // 할 일 추가하기
+            insertTodo: function(e){
+                var content = document.querySelector("#todo__content");
+                var limit = document.querySelector("#todo__limit");
+
+                this.todos.push({content: content.value, limit: limit.value});
+
+                content.value = "";
+                limit.value = "";
+                e.currentTarget.classList.remove("show");
+            },
+
+            // 할 일 삭제하기
+            removeTodo: function(id){
+                for(var i = 0; i < this.todos.length; i++){
+                    var todo = this.todos[i];
+                    
+                    if(todo.id == id){
+                        this.todos.splice(i, 1);
+                        break;
+                    }
+                }
             }
         }
     });
